3de96a2ffe206d157b20d9fb94c88426c11be3df
Vova Ushakov <vu4000@hw.ac.uk>
2025-07-27 14:29:18 +0100

    Tree with stuff

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,20 +1,31 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public List<ItemToSpawn> ItemTable = new List<ItemToSpawn>(); 
+    public List<ItemToSpawn> ItemTable = new List<ItemToSpawn>();
     public float Radius = 1;
+    public float Width = 3; // теперь публичная переменная
     private float timer;
     public float period = 1;
 
-    void Spawn() 
+    void Spawn()
     {
-        Vector3 RandomPosition = Random.insideUnitCircle * Radius;
-        foreach (ItemToSpawn item in ItemTable) 
+        //Vector3 RandomPosition = Random.insideUnitCircle * Radius;
+
+        float capsuleBodyWidth = Width - 2 * Radius;
+
+        float x = Random.Range(-capsuleBodyWidth / 2f, capsuleBodyWidth / 2f);
+        float angle = Random.Range(0f, Mathf.PI * 2);
+        float y = Mathf.Sin(angle) * Random.Range(0f, Radius);
+
+        Vector3 randomLocalPos = new Vector3(x, y, 0);
+        Vector3 RandomPosition = transform.position + randomLocalPos;
+
+        foreach (ItemToSpawn item in ItemTable)
         {
-            if(Random.Range(0f, 100f) <= item.DropChance) 
+            if (Random.Range(0f, 100f) <= item.DropChance)
             {
                 Instantiate(item.ItemPrefab, RandomPosition, Quaternion.identity);
                 break;
@@ -26,16 +37,29 @@ public class Spawner : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(this.transform.position, Radius);
+
+        float radius = Radius;
+
+        Vector3 left = transform.position - Vector3.right * (Width / 2 - radius);
+        Vector3 right = transform.position + Vector3.right * (Width / 2 - radius);
+
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width - radius * 2, radius * 2, 0.01f));
+        Gizmos.DrawWireSphere(left, radius);
+        Gizmos.DrawWireSphere(right, radius);
     }
 
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer > period)
-        {
-            timer = 0;
-            Spawn();
-        }
+        //timer += Time.deltaTime;
+        //if (timer > period)
+        //{
+        //    timer = 0;
+        //    Spawn();
+        //}
+    }
+
+    private void OnMouseDown()
+    {
+        Spawn();
     }
 }
